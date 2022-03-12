2406b72d82d98a8bbb98e36e7c52a45dfb01e77b
Neal Maker <nealmaker@gmail.com>
2022-03-12 10:40:40 -0500

    add PBF prices

--- R/run.R
+++ R/run.R
@@ -8,13 +8,18 @@ library("parallel")
 options(dplyr.summarise.inform=F) # quiets "friendly" warning from summarise()
 
 load("dat-simready.rda")
-dat <- dat[dat$plot == unique(dat$plot)[plot_num],]
+treesgo <- dat[dat$plot == unique(dat$plot)[plot_num],]
 trees <- as_tibble(simtrees_sample)
 paramsg <- params_default
 paramsg$steplength <- 10
 paramsg$endyr <- 40
 paramsg$drate <- .04 # forester's discount rate
 l_drate <- .03 # landowner's discount rate
+
+# #2 log prices from PBF averages
+paramsg$prices$mill_grade2 <-
+  c(470, 170, 220, 190, 400, 320, 370, 160, 490, 290, 400, 160, 290, 450, 220,
+    220, 350, 280, 160, 280, 350, 320, 340, 190, 170)
 
 # for ranking trees
 spp_ranks <-
@@ -27,4 +32,4 @@ source("R/forester-opt.R")
 source("R/landowner-objective.R")
 source("R/landowner-opt.R")
 
-best_comp_pcakage <- land_opt(dat, paramsg)
+best_comp_pcakage <- land_opt(treesgo, paramsg)
