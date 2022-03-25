11a987642af66e47aabd18e698191f93bc1e56dc
Neal Maker <nealmaker@gmail.com>
2022-03-25 09:15:07 -0400

    get hw results & poor quality sw data

--- R/prep-data.R
+++ R/prep-data.R
@@ -1,6 +1,9 @@
 library('tidyverse')
 library('forester')
 
+################################################################################
+## Hardwood data from Paul Smith's
+################################################################################
 dat <- read.csv("data/psc-hw-data.csv")
 dat <- dat %>%
   mutate(stand = 1,
@@ -34,5 +37,32 @@ dat$ht <- predict(ht_model_op, newdata = dat)
 dat <- select(dat, stand, plot, tree, spp, dbh, cr, logs, ba, bal, forest_type,
               site_class, lat, lon, elev, ht, tpa_tree, ba_tree)
 class(dat) <- c("tbl_df", "tbl", "data.frame")
+
+save(dat, file = "dat-simready.rda")
+
+################################################################################
+## Lower quality softwood data from Eagle Ledge
+################################################################################
+load("../eagle-ledge/data/el-trees-fixed.rda")
+
+temp <- el_trees_fixed %>% group_by(plot) %>%
+       summarize(pct_spfr = sum(spp %in% c("fir", "spruce", "hemlock"))/n()) %>%
+       arrange(desc(pct_spfr))
+View(filter(el_trees_fixed, plot %in% temp$plot[temp$pct_spfr == 1]))
+# 3_31 is 4 eh well differentiated; 1_4 is 6 eh-sp-fr; 1_6 is 3 sp;
+# 4_10 is 4 eh pretty similar; 1_10 is 5 sp similar quality range of sizes
+
+new <- filter(el_trees_fixed, plot %in% c("3_31", "1_4", "1_10"))
+
+load("dat-simready.rda")
+new <- select(new, names(dat))
+new <- new %>% mutate(plot = case_when(plot == "3_31" ~ 4,
+                                       plot == "1_4" ~ 5,
+                                       TRUE ~ 6),
+                      stand = 2) %>%
+  arrange(plot)
+new$tree <- (max(dat$tree) + 1):(max(dat$tree) + nrow(new))
+
+dat <- rbind(dat, new)
 
 save(dat, file = "dat-simready.rda")
