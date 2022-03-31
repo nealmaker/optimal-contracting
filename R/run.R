4816c435cba906460a96641d9017a7e000ad7818
Neal Maker <nealmaker@gmail.com>
2022-03-31 07:27:01 -0400

    get sw results

--- R/run.R
+++ R/run.R
@@ -1,6 +1,3 @@
-################################################################################
-plot_num <- 3
-################################################################################
 library('forester')
 library('tidyverse')
 library("rgenoud")
@@ -13,10 +10,10 @@ library("parallel")
 options(dplyr.summarise.inform=F) # quiets "friendly" warning from summarise()
 
 load("dat-simready.rda")
-dat <- dat[dat$plot == unique(dat$plot)[plot_num],]
 dat$dbh <- as.double(dat$dbh)
 dat$cr <- as.double(dat$cr)
 class(dat) <- c("tbl_df", "tbl", "data.frame")
+dat_full <- dat
 # trees <- as_tibble(simtrees_sample)
 # treesgo <- trees[!is.na(trees$spp),]
 paramsg <- params_default
@@ -40,12 +37,27 @@ source("R/forester-objective.R")
 source("R/forester-opt.R")
 source("R/landowner-objective.R")
 source("R/landowner-opt.R")
-source("R/landowner-opt-genoud.R")
+# source("R/landowner-opt-genoud.R")
 source("R/landowner-opt-ga.R")
-source("R/landowner-opt-sa.R")
-source("R/landowner-opt-pso.R")
+# source("R/landowner-opt-sa.R")
+# source("R/landowner-opt-pso.R")
 
-out <- land_opt(dat, paramsg, algo = "ga")
+dat <- dat_full[dat_full$plot == 4,]
+plt4_results <- land_opt(dat, paramsg, algo = "ga")
+save(plt4_results, file = "results/plt4-2022-03-25.rda")
+
+dat <- dat_full[dat_full$plot == 5,]
+plt5_results <- land_opt(dat, paramsg, algo = "ga")
+save(plt5_results, file = "results/plt5-2022-03-26.rda")
+
+dat <- dat_full[dat_full$plot == 6,]
+plt6_results <- land_opt(dat, paramsg, algo = "ga")
+save(plt6_results, file = "results/plt6-2022-03-26.rda")
+
+plt6_res2 <- land_opt(dat, paramsg, algo = "ga")
+plt6_res3 <- land_opt(dat, paramsg, algo = "ga")
+
+save(plt6_results, plt6_res2, plt6_res3, file = "results/plt6-sensitivity.rda")
 
 # testing Forester's Optimizer
 # ptemp <- c(3, .037, 22.6, 17.8, .068)
